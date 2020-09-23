2dfe09e6cf6c39f317a09ae4d1b5695b53dc1ddd
Alexander Chapkouski <alexander.chapkovs@icloud.com>
2020-09-23 01:22:27 -0400

    fix: add it tests

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -1,3 +1,5 @@
+
+
 describe("Inspect Automation Test Store items using chain of commands", () => {
   it("Click on the first item using item header", () => {
     cy.visit("https://automationteststore.com/");
@@ -5,7 +7,7 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
     cy.get('a[href*="product/category&path="]').contains("Skincare").click();
   });
 
-  it.only("Navigating to specific product", () => {
+  it("Navigating to specific product", () => {
     cy.visit("https://automationteststore.com/");
     cy.get('a[href*="product/category&path="]').contains("Makeup").click();
     cy.get('h1 .maintext').click().then(($headerText) => {
@@ -14,4 +16,27 @@ describe("Inspect Automation Test Store items using chain of commands", () => {
         cy.log(headerText);
       });
   });
+
+  it.only("Validate properties of the Contact Us Page", () => {
+    cy.visit("https://automationteststore.com/index.php?rt=content/contact");
+    
+    // Uses cypress commands and chaining
+    cy.contains('#ContactUsFrm','Contact Us Form').find('#field_11').should('contain', 'First name')
+
+    // Uses JQuery approach
+    cy.contains('#ContactUsFrm','Contact Us Form').then(($contactUsForm) => {
+        const firstNameText = $contactUsForm.find('#field_11').text();
+        expect(firstNameText).to.contain('First name:');
+
+        //Embedded commands
+        cy.get('#field_11').then((text) => {
+            cy.log(text);
+            cy.log(text.text());
+            
+        })
+    })
+
+
+  });
+
 });
