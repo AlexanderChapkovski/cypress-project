9b13697736c7b4e1f7f92c64d5ac69d5b2ea3984
Alexander Chapkouski <alexander.chapkovs@icloud.com>
2020-09-27 10:59:49 -0400

    fix: fix contact-us.js file

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -1,21 +1,24 @@
 /// <reference types="cypress" />
 
 describe("Test Contact Us form via WebdriverUni", () => {
-  it.only("Should be able to submit a successful submission via contact-us form", () => {
-    cy.visit("http://webdriveruniversity.com/Contact-Us/contactus.html");
+  it("Should be able to submit a successful submission via contact-us form", () => {
+    cy.visit('http://webdriveruniversity.com/');
+    cy.get('#contact-us').invoke('removeAttr', 'target').click();
     cy.url().should('include', 'contactus');
     cy.title().should('include', 'WebDriver | Contact');
     cy.document().should("have.a.property", "charset").and("eq", "UTF-8");
     cy.get('[name="first_name"]').type("Alex");
     cy.get('[name="last_name"]').type("Chapkouski");
     cy.get('[name="email"]').type("Sanchous@gmail.com");
-    cy.get('[placeholder="Comments"]').type("Vasya");
+    cy.get('[placeholder="Comments"]').type("Vasyliy");
     cy.get('[type="submit"]').click({ force: true });
     cy.get("h1").should("have.text", "Thank You for your Message!");
   });
 
+
   it("Should not be able to submit a successful submission via contact-us form", () => {
-    cy.visit("http://webdriveruniversity.com/Contact-Us/contactus.html");
+    cy.visit('http://webdriveruniversity.com/');
+    cy.get('#contact-us').invoke('removeAttr', 'target').click();
     cy.get('[name="first_name"]').type("Alex");
     cy.get('[name="last_name"]').type("Chapkouski");
     cy.get('[name="email"]').type("Sanchous@gmail.com");
